64dbd057d316cdc6c4fbe9c68b6e0bbb4c66633d
Lumikrynd <WhatIsEmail?>
2025-11-21 20:45:06 +0100

    Day 1 part 2

--- y2022/src/day1/mod.rs
+++ y2022/src/day1/mod.rs
@@ -1,18 +1,29 @@
+use std::collections::BinaryHeap;
+
 mod parsing;
 
 pub fn solve(input: &str) -> String {
     let elfs = parsing::parse(input);
-    let part_1 = solve_part_1(elfs);
+    let part_1 = solve_part_1(&elfs);
+    let part_2 = solve_part_2(&elfs);
 
-    format!("part1: {}", part_1)
+    format!("part1: {}\npart2: {}", part_1, part_2)
 }
 
-fn solve_part_1(elfs: Vec<Elf>) -> String {
+fn solve_part_1(elfs: &Vec<Elf>) -> String {
     let result = elfs.iter()
         .map(|e| e.sum())
         .max()
         .unwrap();
 
+    result.to_string()
+}
+
+fn solve_part_2(elfs: &Vec<Elf>) -> String {
+    let values = elfs.iter().map(|e| e.sum());
+    let sorted = BinaryHeap::from_iter(values);
+
+    let result : u32 = sorted.iter().take(3).sum();
     result.to_string()
 }
 
--- y2022/src/lib.rs
+++ y2022/src/lib.rs
@@ -7,17 +7,21 @@ pub fn solve_day(
     day: Option<String>,
     _test_data: Option<String>,
 ) -> Result<String, Box<dyn Error>> {
-    let day = day.ok_or(AocError::new("Need to set day"))?;
-    let day: u8 = day
-        .parse()
-        .map_err(|e| AocError::new(&format!("Failed parsing day: {}", e)))?;
-
+    let day = parse_day(day)?;
     let input = get_puzzle_input(day)?;
 
     match day {
         1 => Ok(day1::solve(&input)),
         _ => Err(Box::new(AocError::new("Not Implemented"))),
     }
+}
+
+fn parse_day(day: Option<String>) -> Result<u8, Box<dyn Error + 'static>> {
+    let day_string = day.ok_or(AocError::new("Need to set day"))?;
+    let day: u8 = day_string
+        .parse()
+        .map_err(|e| AocError::from_err("Failed parsing day:", e))?;
+    Ok(day)
 }
 
 fn get_puzzle_input(day: u8) -> Result<String, Box<dyn Error>> {
